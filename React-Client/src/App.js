c354443de0b8c0abeb962080f5dea033a529ac2a
Matheews sathurshan <Sathu2014al@gmail.com>
2019-10-18 22:41:10 +0530

    First Commit

---
+++ React-Client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import ProjectIndex from "./component/Defect/ProjectIndex"
+import Layout from "./component/Defect/Layout"
+
+function App() {
+  return (
+    <div>
+    <Layout/>
+    <ProjectIndex/>
+    {/* <h1>hello</h1> */}
+    </div>
+  );
+}
+
+export default App;
